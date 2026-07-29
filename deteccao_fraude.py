308c3b5353e79f0ca443143637b45ad1d55a84bc
Dani <daniellytoledosouto@gmail.com>
2026-07-29 19:15:26 +0100

    RandomForestClassifier

--- deteccao_fraude.py
+++ deteccao_fraude.py
@@ -15,18 +15,20 @@ print(df["Class"].value_counts(normalize=True))
 # 1    0.001727
 # Name: proportion, dtype: float64
 
-# Ao rodar essa função, estamos calculando a proporção de cada tipo de transação. O resultado mostra que 0.001 transações são fraudes. Isso mostra um desbalanceamento entre as classes 0 e 1, não significa que é algo bom. Isso ode ser um problema porque o modelo pode aprender que 1 não é fraude.
-# Neste caso, podemos usar o Recall para identificar quantas fraudes reais ele encontrou.
+# Ao rodar essa função, estamos calculando a proporção de cada tipo de transação. O resultado mostra que 0.001 transações são fraudes. Isso mostra um desbalanceamento entre as classes 0 e 1, não significa que é algo bom.
+# Isso pode ser um problema porque o modelo pode aprender a prever quase sempre a classe 0 (não fraude), ignorando a classe 1 (fraude).
+# O Recall mede a proporção de fraudes reais que o modelo conseguiu identificar.
 
 #----------------------
 
 # Feature Engineering
 # Criando variáveis que ajudam o modelo
-# Por que criar outras variáveis? Nem sempre as variáveis originais como as desse projeto como Time, Amount e V1-V28 são o suficiente, então é melhor criar novas variáveis que vão representar melhor o comportamento dos dados. A ideia é transformar os dados brutos em informações que sejam úteis para o modelo. O modelo não interpreta o contexto, ele só aprende contadores matemáticos.
+# Por que criar outras variáveis? Nem sempre as variáveis originais como as desse projeto como Time, Amount e V1-V28 são o suficiente, então é melhor criar novas variáveis que vão representar melhor o comportamento dos dados. A ideia é transformar os dados brutos em informações que sejam úteis para o modelo.
+# O modelo não entende o significado dos dados; ele aprende padrões matemáticos presentes nas variáveis.
 
 import numpy as np
 
-# Criando uma nova variável chamada Amount_log. log1p aplica uma transformação logarítimica nos valores da coluna Amount. É uma forma de comprimir valores muito grande, diminuindo-os na escala; valores pequenos são menos afetados. Isso evita problemas com valores iguais = 0, já que log = 0 não vai existir. Algumas transações são muito pequenas e outras são muito grandes e isso pode dificultar o aprendizado do modelo porque os valores vão ficar muito espalhados, e o log reduz essa diferença e a distribuição fica mais equilibrada, ajudando o modelo a identificar padrões com mais facilidade.
+# Criando uma nova variável chamada Amount_log. log1p aplica uma transformação logarítimica nos valores da coluna Amount. É uma forma de comprimir valores muito grande, diminuindo-os na escala; valores pequenos são menos afetados. A função log1p(x) calcula log(1 + x), permitindo transformar valores iguais a 0 sem gerar erro, já que log(1) = 0. Algumas transações são muito pequenas e outras são muito grandes e isso pode dificultar o aprendizado do modelo porque os valores vão ficar muito espalhados, e o log reduz essa diferença e a distribuição fica mais equilibrada, ajudando o modelo a identificar padrões com mais facilidade.
 df["Amount_log"] = np.log1p(df["Amount"])
 
 #----------------------
@@ -60,7 +62,7 @@ X_train, X_test, y_train, y_test = train_test_split(
 
 # Prever se a transação é fraude (classe 1) ou não fraude (classe 0)
 # Por que da regressão logistíca? É um dos modelos mais simples e é geralmente usado para base line para servir de modelo para os outros. 
-# O max_inter define 1000 interações para o algoritmo convergir.
+# O max_iter define o número máximo de iterações que o algoritmo poderá executar para encontrar os melhores coeficientes.
 # O model.fit é o modelo aprende padrões a partir dos dados de train, encontrando relações entre as variáveis
 # Model.predict vai fazer previsões dos dados de teste que ele nunca viu, agora é momento que ele vai olhar para os 30% de teste.
 
@@ -73,7 +75,7 @@ y_pred = model.predict(X_test)
 # ao executar o código acima quando max_inter=1000 vai dar um aviso de:
 # ConvergenceWarning: lbfgs failed to converge after 1000 iteration(s) (status=1): STOP: TOTAL NO. OF ITERATIONS REACHED LIMIT
 # não significa que deu erro, mas que o modelo não conseguiu terminar o processo de aprendizado completamente com o número definido no max_inter porque ele não encontrou a melhor solução quando o treinamento foi interrompido, ou seja, o modelo parou porque chegou no máximo permitido, não porque terminou de aprender.
-# neste caso, o que podemos fazer é aumentar o número de interações e melhorar a escola dos dados.
+# neste caso, o que podemos fazer é aumentar o número de interações e melhorar a escala dos dados.
 
 # podemos ver como o modelo se saiu usando o classification_report que é onde conseguimos ver as métricas mais importantes pra nossa avaliação.
 
@@ -90,11 +92,11 @@ print(classification_report(y_test, y_pred))
 # weighted avg      1.00      1.00      1.00     85443
 
 # ANALISANDO O REPORT:
-# na coluna precision, onde mostra a classe 1, podemos ver que entre todas as transações, o modelo recall classificou 86 eram fraudes
-# na reall, 64% de fraudes reais. Ponto crítico porque o modelo está deixando passar 36% de fraude
+# Na coluna precision, onde mostra a classe 1, das transações que o modelo classificou como fraude, 86% realmente eram fraudes.
+# O Recall de 64% indica que o modelo conseguiu identificar 64% das fraudes reais.. Ponto crítico porque o modelo está deixando passar 36% de fraude
 # f1-score de 0.73 que é um equilibrio, uma média harmonica entre precisão e recall, dando uma visão geral da qualidade do modelo
-# accuracy está com 1.00 sendo 100% de acurácia. O que parece bom, mas não bate com os outros dados de precision, recall e f1-score quando interpretamos tudo junto. O accuracy pode acabar sendo alto mesmo sem detectar fraudes.
-# então, o dado mais importante é o recall com 64% de fraudes reais identificadas
+# A accuracy aparece como 1.00 devido ao arredondamento, mas, em problemas desbalanceados, ela pode ser enganosa. O modelo pode apresentar alta accuracy mesmo deixando de identificar muitas fraudes.
+# Então, o dado mais importante é o recall com 64% de fraudes reais identificadas
 
 #----------------------
 
@@ -120,13 +122,13 @@ plt.show()
 
 print("AUC:", roc_auc_score(y_test, y_probs))
 # AUC: 0.9283981824605543
-# área sobre a curva foi de 93 aproximadamente, o que significa que o modelo está muito bom, mas ainda não está perfeito.
+# A AUC foi aproximadamente 0,93 (ou 93%), indicando que o modelo tem boa capacidade de distinguir transações fraudulentas de transações normais, mas ainda não está perfeito.
 
 #----------------------
 
 # Precision Recall Curve
 # Ainda mais importante em problemas de fraudes desbalanceadas
-# Mostra a precisão de acertos em fraudes reais e quantas realmentesão fraudes
+# A curva Precision-Recall mostra a relação entre a precisão (quantas previsões de fraude realmente eram fraudes) e o recall (quantas fraudes reais foram identificadas).
 # A partir do momento que o recall aumenta, o modelo começa a identificar mais fraudes
 
 from sklearn.metrics import precision_recall_curve
@@ -145,18 +147,45 @@ plt.show()
 
 # Undersampling
 # Pega todas as fraudes e seleciona a mesma quantidade de transações, ou seja, reduzimos a classe majoritária 0 para ficar do mesmo tamanho da classe minoritária que é a classe 1
+# Junta as fraudes com todas as transações normais novamente.
 fraudes = df[df["Class"] == 1]
 normais = df[df["Class"] == 0]
 df_under = pd.concat([fraudes, normais])
 
 # Oversamping
-# Cria um dataset equilibrado com a mesma quantidade de frauses e não fraudes.
+# O oversampling aumenta a quantidade de exemplos da classe minoritária para equilibrar o conjunto de dados. Uma das técnicas mais utilizadas é o SMOTE.
 # O modelo aprende melhor a identificar fraudes
 # O problema é que acabamos perdendo dados normais, o que reduz a qualidade do dado, ou seja, nunca é perfeito
 from imblearn.over_sampling import SMOTE
 
-# Gera dados, novas fraudes baseadas nas existentes
+# O smote() cria um dataset equilibrado com a mesma quantidade de frauses e não fraudes.
+# O SMOTE gera exemplos sintéticos da classe minoritária a partir das amostras existentes, em vez de simplesmente duplicá-las.
 # Não perde dados e ainda aumenta a representação da fraude, por outro lado, estamos criando dados artificiais, o que pode incluir ruídos
 smote = SMOTE()
 X_res, y_res = smote.fit_resample(X, y)
+
+#----------------------
+
+from sklearn.ensemble import RandomForestClassifier
+
+rf = RandomForestClassifier(
+    n_estimators=50,
+    max_depth=10,
+    class_weight="balanced",
+    n_jobs=-1,
+    random_state=42
+)
+
+rf.fit(X_train, y_train)
+y_pred_rf = rf.predict(X_test)
+print(classification_report(y_test, y_pred_rf))
+
+#               precision    recall   f1-score  support
+#
+#           0       1.00      1.00      1.00     85295
+#           1       0.74      0.80      0.77       148
+#
+#    accuracy                           1.00     85443
+#   macro avg       0.87      0.90      0.89     85443
+# weighted avg       1.00      1.00      1.00     85443
 
